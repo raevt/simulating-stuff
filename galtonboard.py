0a86257a2d09b636bd9026b559550dd4b582c2e9
Rae Adimer <137109904+raevt@users.noreply.github.com>
2024-07-21 15:15:48 -0400

    Update galtonboard.py
    limit output to only possible positions

--- galtonboard.py
+++ galtonboard.py
@@ -4,8 +4,8 @@
 import random
 
 # Settings
-num_rows = 12 # Set the number of rows on either side of the starting peg
-num_balls = 200 # Set the number of balls on the board
+num_rows = 11 # Set the number of rows on either side of the starting peg
+num_balls = 200 # Set the number of balls/agents on the board
 
 def simulate():
     # Create list
@@ -26,13 +26,20 @@ def output(positions):
     # Record frequencies
     for n in positions:
         freqs[n] += 1
+    # Identify which positions are possible
+    possible_pos = []
+    for i in range(-1*num_rows,num_rows+1):
+        if (num_rows % 2 == 0) and (i % 2 == 0):
+            possible_pos.append(i)
+        elif (num_rows % 2 != 0) and (i % 2 != 0):
+            possible_pos.append(i)
     # Output frequencies
     print("Frequencies")
-    for i in range(-1*num_rows,num_rows+1):
-        print(f"{i}:",end="")
-        for n in range(freqs[i]):
-            print("*",end="")
-        print()
+    for i in possible_pos:
+            print(f"{i}:",end="")
+            for n in range(freqs[i]):
+                print("*",end="")
+            print()
 
 def main():
     output(simulate())
