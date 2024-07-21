01f0ac90bf4572ef58f5f339cc0ec5814d0ed67a
Rae Adimer <137109904+raevt@users.noreply.github.com>
2024-07-21 15:38:54 -0400

    Update galtonboard.py
    add matplotlib

--- galtonboard.py
+++ galtonboard.py
@@ -2,10 +2,12 @@
     Simulate a Galton board!
 """
 import random
+import matplotlib.pyplot as plt
 
 # Settings
-num_rows = 11 # Set the number of rows on either side of the starting peg
-num_balls = 200 # Set the number of balls/agents on the board
+num_rows = 30 # Set the number of rows on either side of the starting peg
+num_balls = 2000 # Set the number of balls/agents on the board
+use_matplotlib = 1 # If yes/1, outputs a matplotlib histogram. Otherwise, prints a horizontal bar chart.
 
 def simulate():
     # Create list
@@ -21,7 +23,7 @@ def simulate():
 def output(positions):
     # Identify which positions are possible
     possible_pos = []
-    for i in range(-1*num_rows,num_rows+1):
+    for i in range(-1 * num_rows, num_rows + 1):
         if (num_rows % 2 == 0) and (i % 2 == 0):
             possible_pos.append(i)
         elif (num_rows % 2 != 0) and (i % 2 != 0):
@@ -34,8 +36,17 @@ def output(positions):
     for n in positions:
         freqs[n] += 1
     # Output frequencies
-    print("Frequencies")
-    for i in possible_pos:
+    if use_matplotlib == 1:
+        # Convert dict to list
+        sum_pos = []
+        for n in possible_pos:
+            sum_pos.append(freqs[n])
+        # for a line rather than histogram, use: plt.plot(possible_pos,sum_pos)
+        plt.hist(possible_pos, len(possible_pos), weights=sum_pos)
+        plt.show()
+    else:
+        print("Frequencies")
+        for i in possible_pos:
             print(f"{i}:",end="")
             for n in range(freqs[i]):
                 print("*",end="")
